b20a06a567e01351c9fca04a6edfe33efda69ab3
Andreina Di Brigida <Akiarakirax3@gmail.com>
2025-12-11 16:46:29 -0400

    Add astro dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [alpinejs(), sitemap(), robotsTxt()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
